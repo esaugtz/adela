afa2894bc3d49ac601749d0a39a28d09abc97782
Frank <akura11.tt@gmail.com>
2016-11-24 13:39:09 -0600

    Retirando comentarios de prueba temporalmente

--- spec/features/organization_manages_profile_spec.rb
+++ spec/features/organization_manages_profile_spec.rb
@@ -36,9 +36,6 @@ feature Organization, 'manages profile:' do
     @not_my_organization = FactoryGirl.create(:organization)
 
     visit profile_organization_path(@not_my_organization)
-    # Nota: Mejorar prueba de backend para que no truene con cambios de front
-    # expect(page).not_to have_content 'Descripción'
-    # expect(page).not_to have_content 'URL Logo'
     expect(current_path).to eq(organization_path(@not_my_organization))
   end
 end
